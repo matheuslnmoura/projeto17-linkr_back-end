a5bcdd8bcccb5df691608aaff21564e7572cd5fb
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-06-14 03:53:12 -0300

    feat: signin route

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
 
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import usersRepository from '../repositories/userRepository.js';
@@ -27,6 +28,39 @@ export async function signup(req, res) {
 
     return res.status(201).json({
       message: 'User created successfully',
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+}
+
+export async function signin(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = (await usersRepository.getUserByEmail(email)).rows[0];
+
+    if (!user) {
+      return res.status(401).json({
+        message: 'Email or password is incorrect',
+      });
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: 'Email or password is incorrect',
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: '12h',
+    });
+
+    return res.status(200).json({
+      message: 'User logged in successfully',
+      token,
     });
   } catch (error) {
     return res.status(500).json(error);
