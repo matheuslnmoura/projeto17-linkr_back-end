d8d04f546c5fc08fe48cb4d4db7ed0467749137a
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-06-16 19:15:23 -0300

    feat: post and hashtags relations

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -11,6 +11,7 @@
 /* eslint-disable no-empty */
 import urlMetadata from 'url-metadata';
 import postsRepository from '../repositories/postsRepository.js';
+import hashtagRepository from '../repositories/hashtagRepository.js';
 
 export async function publishPost(req, res) {
   const { url, description } = req.body;
@@ -31,7 +32,33 @@ export async function publishPost(req, res) {
       imageUrl,
     };
     // eslint-disable-next-line import/no-named-as-default-member
-    const result = await postsRepository.insertPost(publish);
+    const hashtags = description
+      .match(/#\w+/g)
+      .map((hashtag) => hashtag.toLowerCase());
+    console.log({ hashtags });
+    const post = (await postsRepository.insertPost(publish)).rows[0];
+    await Promise.all(
+      hashtags.map(async (hashtag) => {
+        const hashtagExist = (await hashtagRepository.getHashtagByName(hashtag))
+          .rows[0];
+        if (hashtagExist) {
+          //FIXME: erro quando tem hashtag que ja existe
+          //erro ao publicar error: insert or update on table "post_hashtags" violates foreign key constraint "post_hashtags_hashtag_id_fkey"
+          await postsRepository.insertPostHashtagRelation(
+            post.id,
+            hashtagExist.id,
+          );
+        } else {
+          const newHashtag = (await hashtagRepository.insertHashtag(hashtag))
+            .rows[0];
+          await postsRepository.insertPostHashtagRelation(
+            post.id,
+            newHashtag.id,
+          );
+        }
+      }),
+    );
+
     res.sendStatus(201);
   } catch (e) {
     console.log('erro ao publicar', e);
--- repositories/hashtagRepository.js
+++ repositories/hashtagRepository.js
@@ -2,14 +2,17 @@
 import connection from '../db.js';
 
 async function getHashtagByName(hashtag) {
-  return connection.query(`
+  return connection.query(
+    `
     SELECT posts.*
     FROM posts
     JOIN post_hashtags ON post_hashtags.post_id = posts.id
     JOIN hashtags ON hashtags.id = post_hashtags.hashtag_id
     WHERE hashtags.name ILIKE $1
     AND posts.is_deleted = false
-  `, [hashtag]);
+  `,
+    [hashtag],
+  );
 }
 
 async function getMostUsedHashtags() {
@@ -22,9 +25,19 @@ async function getMostUsedHashtags() {
   `);
 }
 
+async function insertHashtag(hashtag) {
+  return connection.query(
+    `
+    INSERT INTO hashtags (name) VALUES ($1) RETURNING *;
+  `,
+    [hashtag],
+  );
+}
+
 const hashtagRepository = {
   getHashtagByName,
   getMostUsedHashtags,
+  insertHashtag,
 };
 
 export default hashtagRepository;
