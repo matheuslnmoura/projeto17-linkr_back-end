bcd35ba79a7d935e2fe2007c8bf5f475b9f957f6
Rafael <rafinha_fern@hotmail.com>
2022-06-17 02:47:41 -0300

    fix: commit with no alterations on main

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 import express, { json } from 'express';
-import { Server } from 'socket.io';
-import http from 'http';
 import cors from 'cors';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
@@ -10,23 +8,12 @@ import dotenv from 'dotenv';
 import router from './routers/masterRouter.js';
 
 const app = express();
-
 app.use(cors());
 app.use(json());
 dotenv.config();
 
 app.use(router);
 
-const httpServer = http.createServer(app);
-
-const io = new Server(httpServer);
-
-httpServer.listen(process.env.PORT_SOCKET, () => {
-  console.log(chalk.bold.blue('Server HTTP running on port', process.env.PORT_SOCKET));
-});
-
 app.listen(process.env.PORT, () => {
-  console.log(chalk.bold.blue('Server HTTPS running on port', process.env.PORT));
+  console.log(chalk.bold.blue('Server running on port', process.env.PORT));
 });
-
-export default { httpServer, io };
