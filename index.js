7d9a58847b0799db94df4ab8d7e1c51b2d5408bc
Rafael <rafinha_fern@hotmail.com>
2022-06-17 02:29:43 -0300

    feat: socket listener with socket.io

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
 import express, { json } from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 import cors from 'cors';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
@@ -8,12 +10,23 @@ import dotenv from 'dotenv';
 import router from './routers/masterRouter.js';
 
 const app = express();
+
 app.use(cors());
 app.use(json());
 dotenv.config();
 
 app.use(router);
 
-app.listen(process.env.PORT, () => {
-  console.log(chalk.bold.blue('Server running on port', process.env.PORT));
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer);
+
+httpServer.listen(process.env.PORT_SOCKET, () => {
+  console.log(chalk.bold.blue('Server HTTP running on port', process.env.PORT_SOCKET));
 });
+
+app.listen(process.env.PORT, () => {
+  console.log(chalk.bold.blue('Server HTTPS running on port', process.env.PORT));
+});
+
+export default { httpServer, io };
