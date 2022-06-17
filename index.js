fd54f12a6d75b1b4e62b5983a8e0a058e26c9fb0
Rafael <rafinha_fern@hotmail.com>
2022-06-17 05:43:34 -0300

    feat: receive search input from front end.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express, { json } from 'express';
 import cors from 'cors';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from 'socket.io';
 
 import router from './routers/masterRouter.js';
 
@@ -13,7 +15,21 @@ app.use(json());
 dotenv.config();
 
 app.use(router);
+const httpServer = http.createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
 
+io.on('connection', (socket) => {
+  socket.on('search-value', (value) => {
+    console.log(value);
+  });
+});
+httpServer.listen(4005, () => {
+  console.log('coneectadona porta 4005');
+});
 app.listen(process.env.PORT, () => {
   console.log(chalk.bold.blue('Server running on port', process.env.PORT));
 });
