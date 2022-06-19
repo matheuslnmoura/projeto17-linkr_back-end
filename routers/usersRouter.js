780dd0c01d617cef69047067b8bd98bee00dc3c5
Rafael <rafinha_fern@hotmail.com>
2022-06-19 05:12:58 -0300

    fix: add route in user/id router and controller to return name in search click.

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import { Router } from 'express';
+import getUserId from '../controllers/userIdController.js';
 import { getUserPageById } from '../controllers/userPageController.js';
 import { getUsers } from '../controllers/usersController.js';
 
 const userRouter = Router();
 userRouter.get('/users/', getUsers);
 userRouter.get('/users/:id', getUserPageById);
+userRouter.get('/user/:id', getUserId);
 
 export default userRouter;
