3d5b22660cbc6f878614dcc9d490f14873ecfb7a
Matheus Chonge <matheus.chong@gmail.com>
2022-06-21 23:37:31 -0300

    Fixed undefined tooltipText sent to Front

--- db.js
+++ db.js
@@ -3,18 +3,38 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connectionParams = {
-  connectionString: process.env.DATABASE_URL,
+let chachedDB = null;
+let connectionParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
-if (process.env.MODE === 'PROD') {
-  connectionParams.ssl = {
-    rejectUnauthorized: false,
-  };
+async function connectDB() {
+  if (chachedDB) {
+    return chachedDB;
+  }
+
+  if (process.env.DATABASE_URL) {
+    connectionParams = {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    };
+  }
+
+  const { Pool } = pg;
+
+  const db = new Pool(connectionParams);
+
+  await db.connect();
+
+  chachedDB = db;
+
+  return db;
 }
 
-const { Pool } = pg;
-
-const connection = new Pool(connectionParams);
+const connection = await connectDB();
 
 export default connection;
