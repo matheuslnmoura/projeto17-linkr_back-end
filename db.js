9bb349a6f230e0fe832f0de11eaca154b1856555
Rafael <rafinha_fern@hotmail.com>
2022-06-22 13:54:21 -0300

    fix:db connection

--- db.js
+++ db.js
@@ -15,17 +15,6 @@ if (process.env.MODE === 'PROD') {
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = 'Otempoerei.';
-const host = 'localhost';
-const port = 5432;
-const database = 'projeto17_backup';
+const connection = new Pool(connectionParams);
 
-const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database,
-});
 export default connection;
