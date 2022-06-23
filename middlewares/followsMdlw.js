51ab30b11fdc0aa9e6d756a7a215cd90168c15c1
Rafael <rafinha_fern@hotmail.com>
2022-06-22 21:37:18 -0300

    feat: middleware to verify if user follow someone and break if don't.

---
+++ middlewares/followsMdlw.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/extensions
+import { searchFollow } from '../repositories/followRepository.js';
+
+// eslint-disable-next-line consistent-return
+export default async function followsMdw(req, res, next) {
+  const userId = res.locals.user.id;
+  try {
+    const follows = await searchFollow(userId);
+    if (follows.rowCount === 0) {
+      const msg = {
+        message: 'You don\'t follow anyone yet.Search for new friends!',
+      };
+      return res.send(msg);
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('erro follow middleware', e);
+    return res.sendStatus(500);
+  }
+  next();
+}
