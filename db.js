ebb6d717daefc8cb5ce96cd1bf7f1202ff12a057
Matheus Chonge <matheus.chong@gmail.com>
2022-06-24 16:17:37 -0300

    now post show repost count

--- db.js
+++ db.js
@@ -3,37 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let chachedDB = null;
-let connectionParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const connectionParams = {
+  connectionString: process.env.DATABASE_URL,
 };
 
-async function connectDB() {
-  if (chachedDB) {
-    return chachedDB;
-  }
-
-  if (process.env.DATABASE_URL) {
-    connectionParams = {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    };
-  }
-
-  const { Pool } = pg;
-
-  const db = new Pool(connectionParams);
-
-  await db.connect();
-
-  chachedDB = db;
-
-  return db;
+if (process.env.MODE === 'PROD') {
+  connectionParams.ssl = {
+    rejectUnauthorized: false,
+  };
 }
-const connection = await connectDB();
+
+const { Pool } = pg;
+
+const connection = new Pool(connectionParams);
 
 export default connection;
